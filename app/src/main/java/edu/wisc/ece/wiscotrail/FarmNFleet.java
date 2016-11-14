6f50209796497c2fc50c637bab3d678bf9b22ea7
lewiskunik <lkunik@wisc.edu>
2016-11-13 18:10:58 -0600

    changed some TextViews and saved party leader name var to UserVars

--- app/src/main/java/edu/wisc/ece/wiscotrail/FarmNFleet.java
+++ app/src/main/java/edu/wisc/ece/wiscotrail/FarmNFleet.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class FarmNFleet extends AppCompatActivity {
 
@@ -11,6 +12,19 @@ public class FarmNFleet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_nfleet);
+
+        TextView numOxen = (TextView)findViewById(R.id.numOxen);
+        TextView numFood = (TextView)findViewById(R.id.numFood);
+        TextView numAlcohol = (TextView)findViewById(R.id.numAlcohol);
+        TextView numClothing = (TextView)findViewById(R.id.numClothing);
+        TextView numAmmunition = (TextView)findViewById(R.id.numAmmunition);
+
+        numOxen.setText(Integer.toString(UserVars.num_oxen));
+        numFood.setText(UserVars.food_lbs + " pounds");
+        numAlcohol.setText(UserVars.alcohol_gallons + " gallons");
+        numClothing.setText(UserVars.num_clothes + " sets");
+        numAmmunition.setText(Integer.toString(UserVars.ammunition));
+
     }
 
     public void goNext(View view){
--- app/src/main/java/edu/wisc/ece/wiscotrail/PickLeader.java
+++ app/src/main/java/edu/wisc/ece/wiscotrail/PickLeader.java
@@ -4,18 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PickLeader extends AppCompatActivity {
+
+    EditText nameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_leader);
+
+        nameField = (EditText)findViewById(R.id.editText);
     }
 
     public void goNext(View view){
-        //TODO: Save variable for party leader name
-        Intent intent = new Intent(this, PickParty.class);
-        startActivity(intent);
+        if(nameField.getText().length() > 0) {
+            UserVars.partyLeader = nameField.getText().toString();
+            Intent intent = new Intent(this, PickParty.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),
+                    "Please enter in a name!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
