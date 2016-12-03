f0370b417d43ff9789f917a494315f4e1095a127
jjhaefner <jjhaefner@wisc.edu>
2016-12-03 15:25:57 -0600

    Added supplies viewing and added back buttons to health and supplies.

--- app/src/main/java/edu/wisc/ece/wiscotrail/ViewSupplies.java
+++ app/src/main/java/edu/wisc/ece/wiscotrail/ViewSupplies.java
@@ -2,6 +2,8 @@ package edu.wisc.ece.wiscotrail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewSupplies extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class ViewSupplies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_supplies);
+
+        TextView numOxen = (TextView)findViewById(R.id.suppliesNumOxen);
+        TextView numFood = (TextView)findViewById(R.id.suppliesNumFood);
+        TextView numAlcohol = (TextView)findViewById(R.id.suppliesNumAlcohol);
+        TextView numClothing = (TextView)findViewById(R.id.suppliesNumClothing);
+        TextView numAmmunition = (TextView)findViewById(R.id.suppliesNumAmmunition);
+
+        numOxen.setText(Integer.toString(UserVars.num_oxen));
+        numFood.setText(UserVars.food_lbs + " pounds");
+        numAlcohol.setText(UserVars.alcohol_gallons + " gallons");
+        numClothing.setText(UserVars.num_clothes + " sets");
+        numAmmunition.setText(Integer.toString(UserVars.ammunition));
+    }
+
+    public void backToDash(View view){
+        finish();
     }
 }
